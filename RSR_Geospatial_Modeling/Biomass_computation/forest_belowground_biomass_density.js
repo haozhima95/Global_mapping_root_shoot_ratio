12e2e62df80b6622faded84f3426eb8dbcbf1a6f
Haozhi <34656389+haozhima95@users.noreply.github.com>
2021-02-05 21:10:20 +0800

    Create forest_belowground_biomass_density.js

---
+++ RSR_Geospatial_Modeling/Biomass_computation/forest_belowground_biomass_density.js
@@ -0,0 +1,42 @@
+// This script is used for updating the belowground biomass layers of trees
+
+// Load the data
+
+// Set the worldwide polygon.
+var unboundedGeo = ee.Geometry.Polygon([-180, 88, 0, 88, 180, 88, 180, -88, 0, -88, -180, -88], null, false);
+
+// Set the rout path
+
+var st = 'your directory/';
+// AgB
+
+var treeagb = ee.Image(st+'abovetreebiomass_20200501');
+
+// Loop
+
+for(var i = 1; i< 11; i++){
+  // load the rsr layers.
+  var rsr = ee.Image(st+'forest_rmf_model_aggregate_first_'+i+'_20200517');
+      rsr = rsr.divide(ee.Image(100).subtract(rsr));
+      Map.addLayer(rsr);
+  // Get the belowground biomass density.
+  var bgb = treeagb.multiply(rsr);
+  
+  // Mask the layer
+      bgb = bgb.mask(rsr);
+      bgb = bgb.updateMask(1);
+      //Map.addLayer(bgb);
+  // Export the layer
+  Export.image.toAsset({
+    image:bgb,
+    description:'forest_belowground_biomass_density_'+i,
+    assetId:st+'forest_belowground_biomass_density_'+i+'_20200501',
+    region: unboundedGeo,
+    crs:'EPSG:4326',
+    crsTransform:[0.008333333333333333,0,-180,0,-0.008333333333333333,90],
+	  maxPixels: 1e13
+  });
+  
+}
+
+// Another option here that you could avoid the 'for' loop. Instead, map with a function can be applied to a list of data sources. Thus, you could map and aggregate into a imagecollection. This image collection will be aggregated by mean and std. 
